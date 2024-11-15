fc953550e9bd266dd445d5455d3d7f9b5ac9ae64
Quoc Hung <truongquochung312+1@gmail.com>
2024-11-15 19:17:05 +0000

    chore(deps): add body parser

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import compression from 'compression';
 import { exampleRouter } from './routes/example';
 import { defaultValueMiddleware } from './utils';
+import bodyParser from 'body-parser';
 
 export async function createApp(): Promise<Express> {
   const app = express();
@@ -21,7 +22,8 @@ export async function createApp(): Promise<Express> {
     }),
   );
   app.use(cors());
-  app.use(defaultValueMiddleware);
+  app.use(bodyParser.json());
+  // app.use(defaultValueMiddleware);
 
   /* Apply API routes */
   app.use('/example', exampleRouter);
